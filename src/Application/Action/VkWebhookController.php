c1023876453a752f7d6ee83999a7e42dc3388927
Sakharov <s.sakharov@freshauto2.ru>
2023-12-28 07:24:21 +0300

    add certbot

--- src/Application/Action/VkWebhookController.php
+++ src/Application/Action/VkWebhookController.php
@@ -22,7 +22,7 @@ class VkWebhookController extends AbstractController
         $data = $this->request->getParsedBody();
         $this->logger->info('EVENT.VK', $data ?: []);
         if (isset($data['type']) && $data['type'] === 'confirmation') {
-            return $this->getResponse('a72fb1d7');
+            return $this->getResponse('81b98d51');
         }
 //        $this->vkService->publishToQueue($this->request->getParsedBody());
         return $this->getResponse('ok');
